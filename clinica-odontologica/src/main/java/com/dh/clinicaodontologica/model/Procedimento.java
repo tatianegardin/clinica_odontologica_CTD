bb16caa093f8744d14888e36ecaf2e114ad93b48
Miguel Oduber <miguel.oduber@gmail.com>
2022-08-31 22:43:10 -0300

    updating files

--- clinica-odontologica/src/main/java/com/dh/clinicaodontologica/model/Procedimento.java
+++ clinica-odontologica/src/main/java/com/dh/clinicaodontologica/model/Procedimento.java
@@ -1,4 +1,16 @@
 package com.dh.clinicaodontologica.model;
+<<<<<<< Updated upstream
 
 public class Procedimento {
 }
+=======
+import lombok.*;
+
+@Getter;
+@Setter;
+@NoArgs
+@NoArgs
+public class Procedimento {
+
+}
+>>>>>>> Stashed changes
