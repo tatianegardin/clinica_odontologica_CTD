61c43519c42eb735703f60893a29e351c79f33de
eddgh <eddmundosjb@gmail.com>
2022-09-02 16:28:18 -0300

    Atualizando classe Endereco com lombok

--- clinica-odontologica/src/main/java/com/dh/clinicaodontologica/model/Endereco.java
+++ clinica-odontologica/src/main/java/com/dh/clinicaodontologica/model/Endereco.java
@@ -1,5 +1,12 @@
 package com.dh.clinicaodontologica.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
     private Long id;
     private String rua;
@@ -7,61 +14,4 @@ public class Endereco {
     private String bairro;
     private String cidade;
     private String estado;
-
-    public Endereco(Long id, String rua, String numero, String bairro, String cidade, String estado) {
-        this.id = id;
-        this.rua = rua;
-        this.numero = numero;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.estado = estado;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
 }
