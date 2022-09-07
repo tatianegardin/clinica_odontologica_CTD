8ad945cec8ee22fbe9fc66561f10ae84ad51deaf
Tatiane Gardin <contatotatianegardin@hotmail.com>
2022-09-06 22:25:03 -0300

    feat: criação do relacionamento entre as tabelas (#51)

--- clinica-odontologica/src/main/java/com/dh/clinicaodontologica/model/Procedimento.java
+++ clinica-odontologica/src/main/java/com/dh/clinicaodontologica/model/Procedimento.java
@@ -2,13 +2,26 @@ package com.dh.clinicaodontologica.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@Entity
 public class Procedimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
     private String tipo;
+
+    @Column(nullable = false)
     private String descricao;
+
+    @OneToMany(mappedBy = "dentista")
+    List<Consulta> consultaList;
 }
