f3e673839fdff1eb6d4e863df0549ab28b2bc84a
Miguel Oduber <miguel.oduber@gmail.com>
2022-09-20 18:55:33 -0300

    listing dentist

--- clinica-odontologica/src/main/java/com/dh/clinicaodontologica/controller/DentistaController.java
+++ clinica-odontologica/src/main/java/com/dh/clinicaodontologica/controller/DentistaController.java
@@ -1,6 +1,7 @@
 package com.dh.clinicaodontologica.controller;
 
 import com.dh.clinicaodontologica.dto.dentista.DentistaRequestDto;
+import com.dh.clinicaodontologica.dto.dentista.DentistaResponseDto;
 import com.dh.clinicaodontologica.service.impl.DentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,6 +26,10 @@ public class DentistaController {
     public ResponseEntity<DentistaResponseDto> atualizarDentista(@RequestBody DentistaRequestDto dentistaRequestDto, @PathVariable long id) {
         DentistaResponseDto dentistaRequestDto1 = dentistaService.atualizarDentista(dentistaRequestDto, id);
         return ResponseEntity.ok().body(dentistaRequestDto1);
+    }
+    @GetMapping
+    public ResponseEntity<List<DentistaResponseDto>> listarTodos() {
+        return ResponseEntity.ok().body(dentistaService.listarTodos());
     }
 
 }
