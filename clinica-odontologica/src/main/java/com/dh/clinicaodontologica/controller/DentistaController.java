31f414478dfac1ece47317959db662b04ebb06ea
Miguel Oduber <miguel.oduber@gmail.com>
2022-09-20 18:46:13 -0300

    Registering Dentist data

--- clinica-odontologica/src/main/java/com/dh/clinicaodontologica/controller/DentistaController.java
+++ clinica-odontologica/src/main/java/com/dh/clinicaodontologica/controller/DentistaController.java
@@ -1,5 +1,26 @@
 package com.dh.clinicaodontologica.controller;
 
+
+import com.dh.clinicaodontologica.service.impl.DentistaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping ("/dentista")
 public class DentistaController {
+
+    @Autowired
+    DentistaService dentistaService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<DentistaResponseDto> salvarDentista(@RequestBody DentistaRequestDto dentistaRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(dentistaService.salvardentista(dentistaRequestDto));
+    }
+
 
 }
