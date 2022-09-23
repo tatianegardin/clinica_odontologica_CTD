1d495bca530d223435d69f2892e08e1ae931b9bd
tatianegardin <contatotatianegardin@hotmail.com>
2022-09-22 21:29:51 -0300

    feat: configuração mysql para subir local

---
+++ clinica-odontologica/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO procedimento (descricao, tipo) VALUES("Manutenção de aparelho", "Ortodontia");
+INSERT INTO procedimento (descricao, tipo) VALUES("Extração do siso", "Exodontia");
+INSERT INTO procedimento (descricao, tipo) VALUES("Abertura de canal", "Endodontia");
+INSERT INTO procedimento (descricao, tipo) VALUES("Ajuste de prótese", "Prótese");
