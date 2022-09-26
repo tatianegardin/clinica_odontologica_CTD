71c237fff8c3f528aed263816aa5551c9223aeb7
eddgh <eddmundosjb@gmail.com>
2022-09-26 17:19:18 -0300

    feat: validações de Endereço finalizadas

--- clinica-odontologica/src/main/java/com/dh/clinicaodontologica/dto/endereco/EnderecoRequestDto.java
+++ clinica-odontologica/src/main/java/com/dh/clinicaodontologica/dto/endereco/EnderecoRequestDto.java
@@ -6,16 +6,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class EnderecoRequestDto {
     //private Long id;
+    @NotBlank(message = "O campo 'rua' não pode ser nulo!")
     private String rua;
+    @NotBlank(message = "O campo 'numero' não pode ser nulo!")
     private String numero;
+    @NotBlank(message = "O campo 'bairro' não pode ser nulo!")
     private String bairro;
+    @NotBlank(message = "O campo 'cidade' não pode ser nulo!")
     private String cidade;
+    @NotBlank(message = "O campo 'estado' não pode ser nulo!")
     private String estado;
 
 }
